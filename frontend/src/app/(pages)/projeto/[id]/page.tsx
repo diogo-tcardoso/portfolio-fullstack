2b8e3e40626fd73b580df562a2ad0849fab388b8
diogo.tcardoso <diocardoso@hotmail.com>
2025-03-23 16:28:28 -0300

    Add react-markdown for better Readme visualization

--- frontend/src/app/(pages)/projeto/[id]/page.tsx
+++ frontend/src/app/(pages)/projeto/[id]/page.tsx
@@ -1,7 +1,7 @@
 import CarroselImagens from "@/components/shared/carrossel-imagens";
 import Container from "@/components/shared/container";
 import Header from "@/components/shared/header"
-import MdContent from "@/components/shared/md-content";
+import Readme from "@/components/projetos/readme";
 import Tecnologias from "@/components/tecnologias/tecnologias";
 import { getProject } from "@/functions/projetos";
 import { getReadme } from "@/functions/readme";
@@ -20,7 +20,7 @@ export default async function ProjectPage(props: {params: Promise<{id: string}>}
                 <h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
                 <CarroselImagens imagens={projeto.imagens.slice(1)}/>
                 <Tecnologias lista={projeto.tecnologias} tamanhoMenor/>
-                <MdContent markdown={readme} />
+                <Readme markdown={readme} />
             </Container>
         </div>
     );
