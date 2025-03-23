ce61130b222ef90e130dea09f2fc09ad9149b0eb
diogo.tcardoso <diocardoso@hotmail.com>
2025-03-22 21:08:57 -0300

    Created projects function

---
+++ frontend/src/functions/projetos.ts
@@ -0,0 +1,26 @@
+import { Projeto, Tipo } from "@core";
+import { httpGet } from "./api";
+
+export async function getProjects() {
+    const projects: Projeto[] = await httpGet("/api/projetos");
+
+    return {
+        todos: projects,
+        get mobile() {
+            return projects.filter((project) => project.tipo === Tipo.MOBILE)
+        },
+        get web() {
+            return projects.filter((project) => project.tipo === Tipo.WEB)
+        },
+        get jogos() {
+            return projects.filter((project) => project.tipo === Tipo.JOGO)
+        },
+        get destaques() {
+            return projects.filter((project) => project.destaque)
+        }
+    }
+}
+
+export async function getProject (id: string): Promise<Projeto | null> {
+    return await httpGet(`/api/projetos/${id}`);
+}
